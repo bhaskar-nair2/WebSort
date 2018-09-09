75dd3c839f5bd7cf6c8d8e31aaf9d5ce943788a5
bhaskar-nair2 <b.bhaskar.nair@gmail.com>
2018-09-09 17:04:55 +0530

    File Upload working

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, jsonify
-from time import sleep
+from flask import Flask, render_template, jsonify, request
+from werkzeug.utils import secure_filename
+from datetime import date
+import os
 
 # TODO:
 # UPLOAD FILE
 # Search Algorithm
 # Yeild data to sockets
 
-UPLOAD_FOLDER = './static/uploads'
-ALLOWED_EXTENSIONS = {'xlsx'}
+UPLOAD_FOLDER = './static/uploads/'
+ALLOWED_EXTENSIONS = ['xlsx', 'png']
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
@@ -31,6 +33,20 @@ def api():
             "sex": "female"
         }])
 
+
+@app.route('/api/file', methods=['POST'])
+def filez():
+    file = request.files['file']
+    filename = str(date.today()) + secure_filename(file.filename)
+    if file and allowed_file(filename):
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    else:
+        print('File Not Valid!')
+    return render_template('index.html')
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 if __name__ == '__main__':
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -8,3 +8,18 @@ function app() {
         }
     })
 }
+
+$('form').on('submit', function () {
+    event.stopPropagation(); // Stop stuff happening
+    event.preventDefault(); // Totally stop stuff happening
+    let file = document.getElementById('fileBox').files[0]; //Files[0] = 1st file
+    let reader = new FileReader();
+    reader.readAsText(file, 'UTF-8');
+    reader.onload = shipOff;
+
+    function shipOff(event) {
+        let result = event.target.result;
+        let fileName = document.getElementById('fileBox').files[0].name; //Should be 'picture.jpg'
+        $.post('/api/file', {data: result, name: fileName});
+    }
+});
